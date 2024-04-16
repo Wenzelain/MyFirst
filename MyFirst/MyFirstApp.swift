e6f40023edbb049bc1b306bdad4460e37579344a
Vaclav Bohac <mamia_2003@volnny.cz>
2024-04-16 21:44:57 +0200

    Initial Commit

---
+++ MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by Vaclav Bohac on 16.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
